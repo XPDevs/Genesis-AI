42f0cd5702b3760c35331971352450d7f42b638a
XPDevs <xpdevscstmspport@gmail.com>
2026-02-27 12:40:00 +0000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -735,13 +735,6 @@ function appendMessage(text, role, isNew = false, imageUrl = null, footerText = 
     speakBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"></path></svg>';
     speakBtn.onclick = () => { if (window.speechSynthesis.speaking) window.speechSynthesis.cancel(); else window.speechSynthesis.speak(new SpeechSynthesisUtterance(processedText)); };
     actionsDiv.appendChild(speakBtn);
-
-    if (isNew && shouldSpeakResponse) {
-        if (window.speechSynthesis.speaking) window.speechSynthesis.cancel();
-        window.speechSynthesis.speak(new SpeechSynthesisUtterance(processedText));
-        shouldSpeakResponse = false;
-    }
-
     const existingLatest = chatBox.querySelectorAll('.message.ai.latest');
     existingLatest.forEach(el => el.classList.remove('latest'));
     div.classList.add('latest');
